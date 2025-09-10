d634a30de29193d0f745e4777289c70354f496d2
DavidKal29 <davidkalmuk65@gmail.com>
2025-09-10 22:46:25 +0200

    Añadimos las rutas básicas de login y register usando jsonwebtokens enviados a través de cookies al frontend

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv').config()
 const pool = require('./db.js')
 const bcrypt = require('bcryptjs')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+JWT_SECRET = process.env.JWT_SECRET
 
 const cookieParser = require('cookie-parser')
 
@@ -16,13 +18,93 @@ app.use(cors({
     credentials: true
 }))
 
-const PORT = process.env.PORT
 
 
 app.get('/',(req,res)=>{
     res.send('Esto funciona')
 })
 
+//Ruta de login
+app.post('/login',async(req,res)=>{
+    try{
+        let {email,password} = req.body
+        const conn = await pool.getConnection()
+        const [user_exists] = await conn.query('SELECT * FROM usuarios WHERE email = ?',[email])
+
+        if (user_exists.length>0) {
+            const equalPassword = await bcrypt.compare(password,user_exists[0].password)
+
+            if (equalPassword) {
+                const id = user_exists[0].id
+                const username = user_exists[0].username
+                const email = user_exists[0].email
+                const user = {id:id,email:email,username:username}
+                
+                const token = jwt.sign(user,JWT_SECRET,{expiresIn:'1h'})
+
+                res.cookie('token',token,{
+                    httpOnly: true,
+                    secure:true,
+                    maxAge: 3600 * 1000,
+                    sameSite:'none'
+                })
+
+                conn.release()
+                res.json({"user":user_exists[0],"message":"Usuario logueado con éxito"})
+            }else{
+                conn.release()
+                res.json({"message":"Contraseña o Email Incorrectos"})
+            }
+        }else{
+            conn.release()
+            res.json({"message":"Contraseña o Email Incorrecto"})
+        }
+    }catch(error){
+        console.log(error);
+        
+        res.status(500).json({message:"Error en login"})
+    }
+})
+
+
+//Ruta de registro
+app.post('/register',async(req,res)=>{
+    try{
+        let {email,username,password} = req.body
+        const conn = await pool.getConnection()
+        const encriptedPassword = await bcrypt.hash(password,10)
+        const [user_exists] = await conn.query('SELECT * FROM usuarios WHERE email = ? or username = ?',[email,username])
+        
+        if (user_exists.length>0) {
+            conn.release()
+            res.json({"message":"El usuario ya existe"})
+        }else{
+            await conn.query('INSERT INTO usuarios (email, username, password) VALUES (?,?,?)',[email,username,encriptedPassword])
+            const [user_exists] = await conn.query('SELECT * FROM usuarios WHERE email = ?',[email])
+            const user = {id:user_exists[0].id,email:user_exists[0].email,username:user_exists[0].username}
+            const token = jwt.sign(user,JWT_SECRET,{expiresIn:'1h'})
+
+            res.cookie('token',token,{
+                httpOnly: true,
+                secure:true,
+                maxAge: 3600 * 1000,
+                sameSite:'none'
+            })
+
+            conn.release()
+            res.json({"user":user,"message":"El usuario ha sido registrado"})
+        }
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:"Error en register"})
+    }
+})
+
+
+
+
+
+const PORT = process.env.PORT
 
 app.listen(PORT,()=>{
     console.log('Escuhando en el puerto', PORT);
